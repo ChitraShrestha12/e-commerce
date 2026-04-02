b3430d9244479ce741514681f623d0c48749e3b4
Chitra Shrestha <ChitraShrestha12.com>
2026-04-02 08:07:23 +0545

    chore: add logo path to categories table

--- database/migrations/2026_04_02_022030_add_icon_path_column_on_categories_table.php
+++ database/migrations/2026_04_02_022030_add_icon_path_column_on_categories_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->softDeletes();
+        Schema::table('categories', function (Blueprint $table) {
+            $table->string('logo_path')->after('name');
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('categories', function (Blueprint $table) {
             //
         });
     }
