ad4b80d127d204ab3f577efbfbb919a83c293aa9
Chitra Shrestha <ChitraShrestha12.com>
2026-04-10 20:02:18 +0545

    feat(product): implement public products

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::with('category')->get();
+        return response()->json([
+            "status" => true,
+            "message" => "Products fetched successfully!",
+            "products" => $products
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\API\AdminController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/signup', [AuthController::class, 'signUp']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::apiResource('admin/products',AdminController::class);
+Route::apiResource('/admin/products',AdminController::class);
+Route::get('/products',[ProductController::class,'index']);
